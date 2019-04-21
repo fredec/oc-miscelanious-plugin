ff69168c90dcc74490b69c1739bea4c533c96183
Frederico Marinho <fredericomarinho@hotmail.com>
2019-04-21 16:09:28 -0300

    - sort_order field

---
+++ diveramkt/miscelanious/updates/builder_table_update_diveramkt_miscelanious_social.php
@@ -0,0 +1,23 @@
+<?php namespace Diveramkt\Miscelanious\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateDiveramktMiscelaniousSocial extends Migration
+{
+    public function up()
+    {
+        Schema::table('diveramkt_miscelanious_social', function($table)
+        {
+            $table->integer('sort_order')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('diveramkt_miscelanious_social', function($table)
+        {
+            $table->dropColumn('sort_order');
+        });
+    }
+}
