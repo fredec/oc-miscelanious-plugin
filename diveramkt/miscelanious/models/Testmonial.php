e8bb5563f134bc9a5c7d9f289e6e0d3e3ef0b7be
fredec <fredericomarinho@hotmail.com>
2019-06-27 16:37:16 -0300

    scope method wth active/deactive

--- diveramkt/miscelanious/models/Testmonial.php
+++ diveramkt/miscelanious/models/Testmonial.php
@@ -20,4 +20,9 @@ class Testmonial extends Model
      * @var string The database table used by the model.
      */
     public $table = 'diveramkt_miscelanious_testmonials';
+
+    public function scopeActive($query)
+    {
+        return $query->where('active', true);
+    }
 }
